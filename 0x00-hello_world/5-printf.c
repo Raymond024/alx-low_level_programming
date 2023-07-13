21a13982fe7e431d6c1f5d5afa696ec8b244d95b
Raymond024 <raymondmugimba2@gmail.com>
2023-07-13 12:06:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
-	 * main entry point
-	 * return: always 0
+	 * main - entry point
+	 * return: always 0 (success)
 	 */
-
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
