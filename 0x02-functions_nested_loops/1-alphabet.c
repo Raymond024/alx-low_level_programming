87e4ee5011ec793062d517b489bece101dbc4517
Raymond024 <raymondmugimba2@gmail.com>
2023-07-19 02:27:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alpabet in lower casse
+ *
+ * Return: always 0 success
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
