aaa65e31aefc83b98700fe4e18ef90186b1483e9
Raymond024 <raymondmugimba2@gmail.com>
2023-08-24 08:27:13 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of the linked list
+ * @h: linked list to be printed
+ * Return: the elements in the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u} %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# A READ ME FILE FOR 0x12-singly_linked_lists
+this fille is is a readme for sindly linked lsts tasks
