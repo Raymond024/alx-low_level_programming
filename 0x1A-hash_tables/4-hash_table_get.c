5442af383fabeaeb50294cfd759497142d91aee1
Raymond024 <raymondmugimba2@gmail.com>
2024-04-12 06:31:50 -0700

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieve value associeted with key
+ * @ht: pointer to table
+ * @key: key to retrive value
+ * Return: value or NULL if key does not exit.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int ind = 0;
+	hash_node_t  *buck;
+	if (!ht || !key || !*key)
+		return (NULL);
+	ind = key_index((const unsigned char *)key, ht->size);
+	buck = ht->array[ind];
+
+	while (buck)
+	{
+		if (!strcmp(key, buck->key))
+			return (buck->value);
+		buck = buck->next;
+	}
+	return (NULL);
+}
